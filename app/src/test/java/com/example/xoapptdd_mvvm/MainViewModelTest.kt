a8ee80aa4877bc387e3ae14fbe120b72aaa9df6b
NumpyGlumpy <khaypov03@mail.ru>
2023-03-13 00:38:34 +0300

    - 001 created repository

---
+++ app/src/test/java/com/example/xoapptdd_mvvm/MainViewModelTest.kt
@@ -0,0 +1,14 @@
+package com.example.xoapptdd_mvvm
+
+import org.junit.Test
+
+class MainViewModelTest {
+
+    @Test
+    fun test() {
+        val communication = TestCommunication()
+        val viewModel = MainViewMode(communication)
+    }
+
+    class TestCommunication :
+}
